c26dfcd9a3f7befce97a2a6f2d8657d747f8c744
David <davidohanlon85@googlemail.com>
2025-08-31 14:36:35 +0100

    Delete unneeded tests

--- client/src/cypress/e2e/home.cy.js
+++
@@ -1,6 +0,0 @@
-describe('Home page', () => {
-    it('should load and display the title', () => {
-      cy.visit('http://localhost:5173'); // adjust if your dev server is different
-      cy.contains('Notely').should('be.visible');
-    });
-  });
